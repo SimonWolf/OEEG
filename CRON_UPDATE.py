85f30044e3330e10e65504b0537d97fa4148cc37
Simon Wolf <simw94@gmail.com>
2026-05-13 14:51:55 +0200

    fix: optimize Leistung processing in CRON_UPDATE and update ertrag data

--- CRON_UPDATE.py
+++ CRON_UPDATE.py
@@ -4,5 +4,6 @@ from src.ertrag import update_ertrag
 leistung = Leistung()
 for s in  ["muensingen", "karlsruhe", "badboll", "mettingen", "holzgerlingen", "tuebingen", "hospitalhof"]:
     leistung.download_days(s,365)
+leistung.optimize()
 
 update_ertrag()
